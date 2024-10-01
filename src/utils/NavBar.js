ccd5fd76a81726e7a7d90bbd90df043c231791b6
03gabriel12 <leandrork12@gmial.com>
2024-10-01 16:13:49 -0500

    e

--- src/utils/NavBar.js
+++ src/utils/NavBar.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function NavBar() {
   return (
     <NavBar className="navBaRight">
-      <NavLink> Holea</NavLink>
+
     </NavBar>
   );
 }
